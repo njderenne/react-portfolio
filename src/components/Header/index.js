c3b8cfc9ec40351b8617528fbaecef2bfcb66664
In Code We Trust <njderenne@gmail.com>
2021-01-26 22:31:02 -0600

    created initial file structure

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Nav from '../Nav';
+
+function Header() {
+    return (
+        <section>
+            <Nav></Nav>
+            <h1>First Load</h1>
+        </section>
+    );
+}
+
+export default Header;
---
+++ src/components/Nav/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Nav() {
+
+    return (
+        <p>hello</p>
+    );
+}
+
+export default Nav;
