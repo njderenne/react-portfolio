3ee4ce82202cf6bd428b396f3648d4961bc2009f
In Code We Trust <njderenne@gmail.com>
2021-01-28 00:02:54 -0600

    added form to contact page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import About from './components/About';
 import Project from './components/Project';
+import Contact from './components/Contact';
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <main>
         <About></About>
         <Project></Project>
+        <Contact></Contact>
       </main>
         
     </div>
