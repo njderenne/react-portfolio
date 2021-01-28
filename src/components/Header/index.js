4bcb6fa3355de508a25d17f7018103d9a552416c
In Code We Trust <njderenne@gmail.com>
2021-01-27 18:50:26 -0600

    formated nav bar and added background hero image

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,10 +3,14 @@ import Nav from '../Nav';
 
 function Header() {
     return (
-        <section>
-            <h1>Nate Derenne</h1>
+        <header className='hero'>
             <Nav></Nav>
-        </section>
+            <div className='vertical-center'>
+                <span className='flex-row'>Hi, my name is<h1 className='flex-row'>Nate Derenne</h1></span>
+                <span className='flex-row'>I am a web developer from Green Bay, WI and currently living in Sun Prairie, WI.</span>
+            </div>
+            <button className='flex-row no-background'><i className='arrow down'></i></button>
+        </header>
     );
 }
 
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -3,11 +3,27 @@ import React from 'react';
 function Nav() {
 
     return (
-        <ul>
-            <li>About Me</li>
-            <li>Projects</li>
-            <li>Contact Me</li>
-            <li>Resume</li>
+        <ul className='flex-row nav-bar'>
+            <li className='mx-2 none'>
+                <a data-testid='about' href='#about' className='none nav'>
+                    About
+                </a>
+            </li>
+            <li className='mx-2 none'>
+                <a data-testid='projects' href='#projects' className='none nav'>
+                    Projects
+                </a>
+            </li>
+            <li className='mx-2 none'>
+                <a data-testid='contact' href='#contact' className='none nav'>
+                    Contact
+                </a>
+            </li>
+            <li className='mx-2 none'>
+                <a data-testid='resume' href='#resume' className='none nav'>
+                    Resume
+                </a>
+            </li>
         </ul>
     );
 }
