07319be11385f65a48bd451074088cf8e2735a80
In Code We Trust <njderenne@gmail.com>
2021-01-27 21:50:13 -0600

    added socials to header

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Header';
 function App() {
   return (
     <div>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
       <header>
         <Header></Header>
       </header>
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -9,7 +9,11 @@ function Header() {
                 <span className='flex-row'>Hi, my name is<h1 className='flex-row'>Nate Derenne</h1></span>
                 <span className='flex-row'>I am a web developer from Green Bay, WI and currently living in Sun Prairie, WI.</span>
             </div>
-            <button className='flex-row no-background'><i className='arrow down'></i></button>
+            <div className='flex-row socials'>
+                <a href="https://github.com/njderenne" className="fa fa-github"></a>
+                <a href="https://www.linkedin.com/in/nathan-derenne-1939801b3/" className="fa fa-linkedin"></a>
+            </div>
+            <button className='flex-row no-background arrow-outline'><i className='arrow down'></i></button>
         </header>
     );
 }
