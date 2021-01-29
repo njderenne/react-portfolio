dd0934c0a8ec40e8f4e5ec555a5618adae840369
In Code We Trust <njderenne@gmail.com>
2021-01-29 17:00:21 -0600

    added links to buttons for live app and github

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -2,19 +2,20 @@ import React from 'react';
 
 function Modal({ onClose, currentProject }) {
 
-    const {title, locations, description} = currentProject;
+    const {title, locations, technology, github, application, description} = currentProject;
 
     return (
 
         <div className="modalBackdrop">
             <div className="modalContainer">
                 <h3 className="modalTitle">{title}</h3>
+                <h4>{technology}</h4>
                 <img src={require(`../../assests/screenshots/${locations}`).default} alt="current category" className='modal-img'/>
                 <p className='modal-description'>
                     {description}
                 </p>
-                <button>GitHub</button>
-                <button>Live App</button>
+                <a href={github} target="_blank"><button>GitHub</button></a>
+                <a href={application} target="_blank"><button>Live App</button></a>
                 <button onClick={onClose} type="button">
                     Close this modal
                 </button>
