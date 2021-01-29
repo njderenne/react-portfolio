97cc8769969bea15e2565b04850232d8ded63543
In Code We Trust <njderenne@gmail.com>
2021-01-29 17:34:49 -0600

    created initial styling for modal

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -9,14 +9,14 @@ function Modal({ onClose, currentProject }) {
         <div className="modalBackdrop">
             <div className="modalContainer">
                 <h3 className="modalTitle">{title}</h3>
-                <h4>{technology}</h4>
+                <h4 className='modal-tech'>{technology}</h4>
                 <img src={require(`../../assests/screenshots/${locations}`).default} alt="current category" className='modal-img'/>
                 <p className='modal-description'>
                     {description}
                 </p>
-                <a href={github} target="_blank"><button>GitHub</button></a>
-                <a href={application} target="_blank"><button>Live App</button></a>
-                <button onClick={onClose} type="button">
+                <a href={github} target="_blank" className='modal-link github'>GitHub</a>
+                <a href={application} target="_blank" className='modal-link live-app'>Live App</a>
+                <button onClick={onClose} type="button" className='modal-button'>
                     Close this modal
                 </button>
             </div>
