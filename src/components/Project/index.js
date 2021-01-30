ce5bc66d4eb2e0f376746698c2b5cb62c5253e77
In Code We Trust <njderenne@gmail.com>
2021-01-29 21:19:18 -0600

    added animations to project cards

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,4 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import Aos from 'aos';
+import 'aos/dist/aos.css';
 import Modal from '../Modal';
 
 function Project() {
@@ -11,6 +13,7 @@ function Project() {
             github: 'https://github.com/MarynaPR/24-chains',
             application: 'https://dry-mesa-09626.herokuapp.com/',
             description: '24 Chains gets its name from the standard frisbee golf basket consisting of 24 chains. This application is a hybrid between the ability to search for a frisbee golf course and a social media page. Users can search for, save, and favorite different courses for quick reference in the future as well as posting reviews and results from there our games. This helps the community find things they both like and dislike about courses, show off they scores they were able to achieve and even find their next favorite frisbee golf course!',
+            animation: 'fade-right',
         },
         {
             title: 'OneStopJob',
@@ -19,6 +22,7 @@ function Project() {
             github: 'https://github.com/frostyausty/OneStopJob',
             application: 'https://frostyausty.github.io/OneStopJob/',
             description: 'This web application was built on the need to localize multiple job posting from a variety of job search platforms. OneStopJob is the solution to that problem, the application pulls jobs from two prominent job search platforms. The two platforms are Adzuna and USAJobs. OneStopJob allows the user to look for jobs that may only be offered on a specific job posting database, on one application resulting in an easier and more efficent job search experience.',
+            animation: 'fade-up',
         },
         {
             title: 'Git it Done!',
@@ -27,6 +31,7 @@ function Project() {
             github: 'https://github.com/njderenne/get-it-done',
             application: 'https://njderenne.github.io/get-it-done/',
             description: 'This is the Git it Done description',
+            animation: 'fade-left',
         },
         {
             title: 'The Tech Blog',
@@ -35,6 +40,7 @@ function Project() {
             github: 'https://github.com/njderenne/tech-blog',
             application: 'https://calm-shelf-50969.herokuapp.com/',
             description: 'This application is a responsive blog webpage used for the tech community. The user has the ability to sign up and log in. Once the user is logged in they can see other user blog posts as well as a dashboard where they can see the blog posts they have created. In the dashboard the user also can create new blog posts, edit a post they have already created or delete a created post. From the homepage or the user dashboard the user will have the ability to view all comments on a blog post and will be directed to a page with just a singular post where they can then add a new comment.',
+            animation: 'fade-right',
         },
         {
             title: 'Oh Snap!',
@@ -43,6 +49,7 @@ function Project() {
             github: 'https://github.com/njderenne/photo-port',
             application: 'https://njderenne.github.io/photo-port/',
             description: 'This is the Oh Snap description',
+            animation: 'fade-up',
         },
         {
             title: 'Social Media API',
@@ -51,6 +58,7 @@ function Project() {
             github: 'https://github.com/njderenne/social-network-api',
             application: 'https://github.com/njderenne/social-network-api',
             description: 'Created a backend API for use in a social media application',
+            animation: 'fade-left',
         }
     ])
 
@@ -62,6 +70,10 @@ function Project() {
         setIsModalOpen(!isModalOpen);
     }
 
+    useEffect(() => {
+        Aos.init({duration: 1000 });
+    })
+
     return (
 
         <section>
@@ -71,7 +83,7 @@ function Project() {
             <h2 className='section-title'>Projects</h2>
             <div className='project-container'>
                 {projects.map((project, i) => (
-                    <div className={`project project-${i+1}`}>
+                    <div data-aos={project.animation} className={`project project-${i+1}`}>
                         <img 
                             src={require(`../../assests/screenshots/${project.locations}`).default} 
                             className='project-image'
