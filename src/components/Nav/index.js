37bf85a40daa9e3d610375ceb13e15eb25ada35f
In Code We Trust <njderenne@gmail.com>
2021-02-01 22:07:58 -0600

    updated resume

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import pdf from '../../assests/files/Nathan_Derenne_Resume.pdf';
+import pdf from '../../assests/files/Nathan_Derenne.pdf';
 
 function Nav(props) {
 
